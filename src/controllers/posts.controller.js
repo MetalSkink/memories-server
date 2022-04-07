19193c7c97c5d94b9dfb5fe0c98649ad0517f5b0
Alucard <xavier_ortega98@hotmail.com>
2022-04-07 14:26:13 -0500

    CRUD de posts terminado

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async(req, res) => {
+    try {
+        const postsMessages = await PostMessage.find();
+        res.status(200).json(postsMessages);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+export const createPost = async(req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message
+    });
+  }
+
+}
+
+export const updatePost = async(req, res) => {
+  const {id: _id} = req.params;
+  const post = req.body;
+  
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(400).send("Invalid ID");
+
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true
+    });
+
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+}
+
+export const deletePost =  async(req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send("Invalid ID");
+
+  try {
+    await PostMessage.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "Post deleted"
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 
-import postRoutes from "./routes/posts.js";
+import postRoutes from "./routes/posts.route.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: "50mb",extended: true }));
 app.use(express.urlencoded({ limit: "30mb",extended: true }));
@@ -15,6 +14,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/posts', postRoutes);
 //Conecction to DB
 mongoose.connect(process.env.BD_CNN, {
     useNewUrlParser: true,
